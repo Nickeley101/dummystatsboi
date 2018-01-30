6d7a531e65a2f1898bf07d134c8a0cf4a5e6d8da
BuildTools <unconfigured@null.spigotmc.org>
2018-01-30 18:13:30 -0500

    '//span[@class="winratio"]/text()' i updated the code to have win rate stuff

--- stats boy.py
+++ stats boy.py
@@ -1,7 +1,7 @@
 from lxml import html
 import requests
-#Ask for who to pull
-summs = str(input("Who? "))
+#Ask for who to pull, eventually replace this with a text file and spit out stats for every name in the file
+summs = str(input("Who?(type summoner name) "))
 #Pull HTML from op.gg if on NA
 whosite = 'http://na.op.gg/summoner/userName=' + summs
 page = requests.get(whosite)
@@ -9,8 +9,21 @@ page = requests.get(whosite)
 tree = html.fromstring(page.content)
 #Tell me rank boyo
 rank = tree.xpath('//span[@class="tierRank"]/text()')
+#format the list item as a str
+rank = rank[0]
 #Tell me LP
 lp = tree.xpath('//span[@class="LeaguePoints"]/text()')
-print(lp)
-final = ' '.join(lp)
-print(final)
+#remove whitespace
+lp = lp[0].strip()
+#tell me win rate stuffs
+#pull wr
+wr = tree.xpath('//span[@class="winratio"]/text()')
+wr = wr[0]
+#pull wins and losses
+wins = tree.xpath('//span[@class="wins"]/text()')
+wins = wins[0]
+lose = tree.xpath('//span[@class="losses"]/text()')
+lose = lose[0]
+#print it
+print(summs)
+print(rank,",", lp,",",wr,",",wins,",",lose)
